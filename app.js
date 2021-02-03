606c1305d30abb99d4b71742f421e62ad7b525e8
joshSteadman <josh.steadman15@gmail.com>
2021-02-03 12:24:55 +0000

    project structure created

---
+++ app.js
@@ -0,0 +1,19 @@
+const http = require("http");
+const express = require('express');
+const fs = require("fs")
+const path = require('path');
+
+
+
+const app = express();
+
+const port = 3000
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname+'/views/index.html'));
+   
+  });
+
+app.listen(port, () => {
+    console.log(`listening on port ${port}`)
+})
