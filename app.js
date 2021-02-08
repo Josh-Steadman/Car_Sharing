ac65ef80b3700ece5dcaf29099607e907e0aea6b
joshSteadman <josh.steadman15@gmail.com>
2021-02-08 16:19:29 +0000

    Build: retrieve and filter data for seacrh requests

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ app.get('/', (req, res) => {
 
   app.post('/', urlencodedParser,function(req,res){
     const user = req.body
-    // console.log(user)
+  
     u = new User()
     u.addUser(user);
     
@@ -45,13 +45,38 @@ app.get('/', (req, res) => {
    
   });
 
+  app.post('/home', urlencodedParser,function(req,res){
+    let tp = []
+    const search = req.body
+    let dest = search.destination.split(',')[0].toLowerCase()
+    let dep = search.departure.split(',')[0].toLowerCase()
+    con.connectToServer(async (err) => {
+      if (err) throw err
+      const db = con.getDb()
+       let trips = db.collection("trips");
+      let t = trips.find({'destination': dest, 'departure': dep});
+      
+      t.forEach(element => {
+        tp.push(element)
+        
+      }).then(function() {
+        console.log(tp)
+        res.render("home", {'trips': tp})
+      })
+      
+  })
+ 
+    
+    // res.sendFile(path.join(__dirname+'/views/home.html'));
+  });
+
   app.get('/trips', (req, res) => {
     let tp = []
     con.connectToServer(async (err) => {
       if (err) throw err
       const db = con.getDb()
        let trips = db.collection("trips");
-      let t = trips.find({'destination': 'bristol'});
+      let t = trips.find({'destination': 'bristol', 'price': '15'});
       
       t.forEach(element => {
         tp.push(element)
@@ -62,9 +87,6 @@ app.get('/', (req, res) => {
       })
       
   })
-  
-    // res.sendFile(path.join(__dirname+'/views/trips.html'));
-      //  res.render("trips", {'trips': t})
      
   });
 
@@ -74,7 +96,7 @@ app.get('/', (req, res) => {
     let t = new Trip()
     t.addTrip(trip);
     
-    res.sendFile(path.join(__dirname+'/views/trips.html'));
+    res.sendFile(path.join(__dirname+'/views/home.html'));
   });
 
 
